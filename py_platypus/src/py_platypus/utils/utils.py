0db58bc2dbddcbb579f9f799bcf7c27322940830
timsliu <timliu92@gmail.com>
2021-07-09 15:50:46 -0700

    started separated pic_3d class; tested random initialization

--- py_platypus/src/py_platypus/utils/utils.py
+++ py_platypus/src/py_platypus/utils/utils.py
@@ -15,3 +15,10 @@ def points_to_area(p1, p2):
             p2 - (x, y) tuple of coordinates for second point'''
 
     return abs((p1[0] - p2[0]) * (p1[1] - p2[1]))
+
+def points_to_volume(p1, p2):
+    '''calculates the volume of a rectangular prism bounded by two points
+    inputs: p1 - (x, y) tuple of coordinates for first point
+            p2 - (x, y) tuple of coordinates for second point'''
+
+    return abs((p1[0] - p2[0]) * (p1[1] - p2[1]) * (p1[2] - p2[2]))
