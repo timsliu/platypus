f518ebe25ab8500486a283df27a3303ac4eaa6c7
timsliu <timliu92@gmail.com>
2021-10-18 21:24:48 -0700

    finished updating existing 2d em value unit tests

--- py_platypus/test/unit_tests/2d_em_value_test.py
+++ py_platypus/test/unit_tests/2d_em_value_test.py
@@ -57,7 +57,7 @@ class Pic2dEmValueTester(unittest.TestCase):
         values
         '''
    
-        print("==== Testing Interpolate Ex field ====")
+        print("\n==== Testing Interpolate Ex field ====")
         # randomly set up electric field and interpolate field at each particle 
         self.pic.ex_edges = np.random.rand(self.pic.ex_edges.shape[0], self.pic.ex_edges.shape[1])
         self.pic.init_x_random() 
@@ -67,8 +67,7 @@ class Pic2dEmValueTester(unittest.TestCase):
         expected = np.zeros(self.num_random_test)
         for i in range(self.num_random_test):
 
-            # randomly place N particles in a shifted quarter grid cell
-            offset = np.random.rand()
+            # randomly place N particles in a grid cell shifted half down
             dy, dx = self.pic.dx
             
             x_rand = np.random.rand()
@@ -78,7 +77,6 @@ class Pic2dEmValueTester(unittest.TestCase):
             self.pic.electron_y[i] = (y_rand + 0.5) * dy
 
             # calculate the expected electric field
-            # TODO CHECK THIS
             expected[i] = (self.pic.ex_edges[0][0] * (1 - x_rand) * (1 - y_rand) +\
                            self.pic.ex_edges[0][1] * x_rand * (1 - y_rand) +\
                            self.pic.ex_edges[1][0] * (1 - x_rand) * y_rand +\
@@ -88,9 +86,12 @@ class Pic2dEmValueTester(unittest.TestCase):
         # mean electric field at edges and mean electric field at particles
         # should be similar
         mean_ex_edges = np.mean(self.pic.ex_edges)
-        mean_ex_parts = np.mean(self.pic.e_particle[:, 0])))
-   
-        self.assertTrue(np.isclose(mean_ex_edges, mean_ex_parts))
+        mean_ex_parts = np.mean(self.pic.e_particle[:, 0])
+  
+        self.assertTrue(
+            np.isclose(mean_ex_edges, mean_ex_parts, rtol=1e-2, atol=1e-2), 
+            "Mean ex edges: {} Mean ex particles: {}".format(mean_ex_edges, mean_ex_parts)
+            )
 
         for i in range(self.num_random_test):
             with self.subTest(i=i):
@@ -105,68 +106,99 @@ class Pic2dEmValueTester(unittest.TestCase):
         '''test interpolating the electric field at each particle from the field
         values'''
     
-        return
-        print("==== Testing Interpolate Ey field ====")
-        self.pic.ey_edges = np.indices(self.pic.ey_edges.shape)[0]
+        print("\n==== Testing Interpolate Ey field ====")
+        self.pic.ey_edges = np.random.rand(self.pic.ey_edges.shape[0], self.pic.ey_edges.shape[1])
         self.pic.init_x_random() 
         self.pic.interpolate_e()
-    
-        print("Average Ey field: {} Average Ey particle: {}".format(
-            np.mean(self.pic.ey_edges), np.mean(self.pic.e_particle[:, 1])))
         
+        # calculate the expected Ey field at the first N particles
         expected = np.zeros(self.num_random_test)
         for i in range(self.num_random_test):
-            offset = np.random.rand() / 2
+
+            # randomly place N particles in a grid cell shifted half right
             dy, dx = self.pic.dx
-            self.pic.electron_x[i] = (1 + offset) * dx 
-            self.pic.electron_y[i] = (1 - offset) * dy 
-    
-            short = 0.5 - offset
-            expected[i] = (self.pic.ex_edges[0][0] * offset * short +\
-                           self.pic.ex_edges[0][1] * (1 - short) * offset +\
-                           self.pic.ex_edges[1][0] * (1 - offset) * short +\
-                           self.pic.ex_edges[1][1] * (1 - offset) * (1 - short)) 
+            
+            x_rand = np.random.rand()
+            y_rand = np.random.rand()
+            
+            self.pic.electron_x[i] = (x_rand + 0.5) * dx 
+            self.pic.electron_y[i] = y_rand * dy
+
+            # calculate the expected electric field
+            expected[i] = (self.pic.ey_edges[0][0] * (1 - x_rand) * (1 - y_rand) +\
+                           self.pic.ey_edges[0][1] * x_rand * (1 - y_rand) +\
+                           self.pic.ey_edges[1][0] * (1 - x_rand) * y_rand +\
+                           self.pic.ey_edges[1][1] * x_rand * y_rand) 
         self.pic.interpolate_e()
-       
+           
+        # mean electric field at edges and mean electric field at particles
+        # should be similar
+        mean_ey_edges = np.mean(self.pic.ey_edges)
+        mean_ey_parts = np.mean(self.pic.e_particle[:, 1])
+
+        # TODO these are not as close as the ex field for no apparent reason
+        self.assertTrue(
+            np.isclose(mean_ey_edges, mean_ey_parts, rtol=1e-2, atol=1e-2), 
+            "Mean ey edges: {} Mean ey particles: {}".format(mean_ey_edges, mean_ey_parts)
+            )
+
         for i in range(self.num_random_test):
-            print("Expected: {} Actual: {}".format(expected[i], self.pic.e_particle[i][0]))
+            with self.subTest(i=i):
+                self.assertTrue(
+                    np.isclose(expected[i], self.pic.e_particle[i][1]), 
+                    "Expected: {} Actual: {}".format(expected[i], self.pic.e_particle[i][1])
+                )
     
         return
     
     def test_interpolate_b(self):
-        '''test interpolate the magnetic field at each particle from the field
-        values'''
-        return
+        '''
+        test interpolating the magnetic field at several test particles
+        from the field values
+        '''
     
-        print("==== Testing Interpolate B field ====")
-        self.pic.bz = np.indices(self.pic.bz.shape)[0]
-        self.pic.init_x_random() 
-        self.pic.interpolate_b() 
-        
-        print("Average B field: {} Average B particle: {}".format(
-            np.mean(self.pic.bz), np.mean(self.pic.b_particle)))
-       
-        values = [[4, 1], [8, 2]]
-    
-        self.pic.bz[0][0] = values[0][0]
-        self.pic.bz[0][1] = values[0][1]
-        self.pic.bz[1][0] = values[1][0]
-        self.pic.bz[1][1] = values[1][1]
-    
-        for i in range(5):
-            offset = np.random.rand() / 2
+        print("\n==== Testing Interpolate B field ====")
+        self.pic.bz = np.random.rand(self.pic.bz.shape[0], self.pic.bz.shape[1])
+        self.pic.init_x_random()
+      
+        # calculate the expected Bz field at the first N particles
+        expected = np.zeros(self.num_random_test)
+        for i in range(self.num_random_test):
+
+            # randomly place N particles in a grid cell shifted half right
             dy, dx = self.pic.dx
-            self.pic.electron_x[0] = (1 + offset) * dx 
-            self.pic.electron_y[0] = (1 - offset) * dy 
-    
-            short = 0.5 - offset
-            expected = (self.pic.bz[0][0] * (1 - short) * short +\
-                        self.pic.bz[0][1] * (1 - short) * (1 - short) +\
-                        self.pic.bz[1][0] * short * short +\
-                        self.pic.bz[1][1] * (1 - short) * short)
-            self.pic.interpolate_b()
+            
+            x_rand = np.random.rand()
+            y_rand = np.random.rand()
+            
+            self.pic.electron_x[i] = x_rand * dx 
+            self.pic.electron_y[i] = y_rand * dy
+
+            # calculate the expected electric field
+            expected[i] = (self.pic.bz[0][0] * (1 - x_rand) * (1 - y_rand) +\
+                           self.pic.bz[0][1] * x_rand * (1 - y_rand) +\
+                           self.pic.bz[1][0] * (1 - x_rand) * y_rand +\
+                           self.pic.bz[1][1] * x_rand * y_rand) 
         
-            print("Expected: {} Actual: {}".format(expected, self.pic.b_particle[0]))
+        self.pic.interpolate_b() 
+        mean_bz_grid = np.mean(self.pic.bz) 
+        mean_bz_parts = np.mean(self.pic.b_particle)
+           
+
+        self.assertTrue(
+            np.isclose(mean_bz_grid, mean_bz_parts, rtol=1e-3, atol=1e-3), 
+            "Mean bz edges: {} Mean bz particles: {}".format(mean_bz_grid, mean_bz_parts)
+            )
+
+        
+        for i in range(self.num_random_test):
+            np.isclose(expected[i], self.pic.bz[i]), 
+            with self.subTest(i=i):
+                self.assertTrue(
+                    np.isclose(expected[i], self.pic.b_particle[i]), 
+                    "Expected: {} Actual: {}".format(expected[i], self.pic.b_particle[i])
+                    )
+          
         return
 
 if __name__ == "__main__":
