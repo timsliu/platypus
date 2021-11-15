f28019ee5503fbea72f5a54f345dd06b6be9ee70
timsliu <timliu92@gmail.com>
2021-11-14 16:26:17 -0800

    added function ev_to_vth for converting between energy and thermal velocity; added necessary constants and applied yapf formatter to constants and math_utils.py

--- py_platypus/src/py_platypus/utils/constants.py
+++ py_platypus/src/py_platypus/utils/constants.py
@@ -1,4 +1,9 @@
 """
-File containing physical constants constants
+File containing physical constants constants. Following python convention,
+the names of variables are capitalized even when the standard is to not
+capitalize. All units are in MKS (meters, kilograms, seconds)
 """
-C = 299792458     # speed of light in meters per second
+SPEED_OF_LIGHT = 299792458  # speed of light in meters per second
+JOULES_PER_EV = 1.602e-19  # joules in an electron volt
+ELECTRON_MASS = 9.109e-31  # mass of electron in kg
+PROTON_MASS = 1.673e-27  # mass of proton in kg
