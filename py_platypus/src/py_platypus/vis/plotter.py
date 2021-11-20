9d6d400c525b5ba8f0c2101497d223efcb214836
timsliu <timliu92@gmail.com>
2021-11-19 23:03:12 -0800

    moved plot_subplots to Plotter class and created function to plot individual plots for each timestep

--- py_platypus/src/py_platypus/vis/plotter.py
+++ py_platypus/src/py_platypus/vis/plotter.py
@@ -7,6 +7,7 @@ import os
 import pickle
 
 import py_platypus as plat
+from py_platypus.vis import vis_util as vis_util
 
 class Plotter:
     def __init__(self, name, params):
@@ -18,6 +19,7 @@ class Plotter:
         self.data_dir = os.path.join(
             plat.PLATYPUS_HOME, "py_platypus/out/{}/data".format(self.params["name"]))
 
+        self.subplots = []
 
     def get_files(self, id_str):
         '''get all files with a certain id string'''
@@ -34,7 +36,7 @@ class Plotter:
         
         return target_files, steps
 
-    def plot_series(self, suffixes, out_name, x_axis, y_axis, title, subplotter):
+    def plot_series(self, suffixes, out_name, x_axis, y_axis, title, subplotter, animate=False, plot_all=False):
         '''helper function to plot a data series across several time steps
         inputs: suffixes - identifier at end of pickle files to parse
                 out_name - name of output file with .png extension
@@ -42,6 +44,8 @@ class Plotter:
                 y_axis - y_axis for charts
                 title - title for each subplot
                 subplotter - function for plotting a subplot'''
+        
+        self.subplots = [] # list of axs objects for each subplot
 
         values    = []    # array holding data for each subplot
         all_files = []    # 2D array with suffixes by subplots elements
@@ -64,7 +68,7 @@ class Plotter:
         graph_file_name = os.path.join(self.out_dir, out_name)
         
         # plot the subplots
-        plat.vis_util.plot_subplots(
+        self.plot_subplots(
             graph_file_name,
             values, 
             x_axis,
@@ -73,6 +77,152 @@ class Plotter:
             subplotter,
             steps=steps
         )
+
+        # optionally create an animation
+        if animate:
+            self.plot_animation(
+                graph_file_name,
+                values, 
+                x_axis,
+                y_axis,
+                title,
+                subplotter,
+                steps=steps
+        )
+
+        # optionally plot separate plots
+        if plot_all:
+            self.plot_all_plots(
+                graph_file_name,
+                values, 
+                x_axis,
+                y_axis,
+                title,
+                subplotter,
+                steps=steps
+        )
+
+    def plot_all_plots(self, filename, data, x_axis, y_axis, title, 
+                   subplotter, log=False, legend=None, steps=None, 
+                   zero=False):
+        '''plot a single chart for each timestep
+        inputs: filename - full path to output filename
+                data - 2d array of data; dimension 0 is for each plot, 
+                       dimension 1 is for multiple data series on a plot
+                x-axis - name of x-axis
+                y-axis - name of y-axis
+                title - figure title
+                log - display y as log plot
+                legend - list of strings labeling the data
+                steps - list of time steps the data is from'''
+
+        plots = len(data)
+        # call function to get upper and lower figure limits
+        lim_neg, lim_pos = vis_util.get_limits(data, zero, subplotter)
+       
+        # create filename_{}.png from filename.png so that step can be added
+        # to the name of each tile
+        dot_index = filename.find(".")
+        filename_format = filename[:dot_index] + "_{}" + filename[dot_index:]
+        
+        if legend is None:
+            legend = plots * [None]
+
+        for i in range(plots):
+            fig, axs = plt.subplots(1, 1, constrained_layout=True, squeeze=False)
+            axs[0, 0] = subplotter(axs[0, 0], data[i], legend[i], (lim_neg, lim_pos))
+            
+            # add optional features
+            if legend is not None:
+                axs[0, 0].legend()
+            
+            if log: 
+                axs[0, 0].yscale("log")
+            
+            axs[0, 0].grid(True)
+    
+            # set the axis labels
+            for ax in axs.flat:
+                ax.set_xlabel(x_axis, fontsize = 10)
+                ax.set_ylabel(y_axis, fontsize = 10)
+    
+            # add figure title
+            fig.suptitle(title)
+       
+            # save file
+            plt.savefig(filename_format.format(i), dpi=800)
+    
+        return
+
+
+    def plot_subplots(self, filename, data, x_axis, y_axis, title, 
+                   subplotter, log=False, legend=None, steps=None, 
+                   zero=False):
+        '''plot a single chart with multiple subplots
+        inputs: filename - full path to output filename
+                data - 2d array of data; dimension 0 is for each subplot, 
+                       dimension 1 is for multiple lines on a subplot
+                x-axis - name of x-axis
+                y-axis - name of y-axis
+                title - figure title
+                log - display y as log plot
+                legend - list of strings labeling the data
+                steps - list of time steps the data is from'''
+    
+        subplots = len(data)                       # number of subplots
+        rows, cols, = vis_util.get_subplot_config(subplots) # subplot arrangement
+        fig, axs = plt.subplots(rows, cols, constrained_layout=True, squeeze=False)
+    
+        # call function to get upper and lower figure limits
+        lim_neg, lim_pos = vis_util.get_limits(data, zero, subplotter)
+    
+        if legend is None:
+            legend = subplots * [None]
+    
+        # iterate through subplots
+        for i in range(rows * cols):
+            # location in the subplot grid
+            col = i % cols
+            row = int(np.floor(i/cols))
+          
+            # remove axis for unused subplots 
+            if i >= subplots:
+                axs[row, col].set_axis_off()
+                continue
+    
+            # call function to plot the data
+            axs[row, col] = subplotter(axs[row, col], data[i], legend[i], (lim_neg, lim_pos))
+           
+            # add subplot title if available
+            if steps is not None: 
+                axs[row, col].set_title("Step {}".format(steps[i]), fontsize=10)
+            
+            # add optional features
+            if legend[i] is not None:
+                axs[row, col].legend()
+            
+            if log: 
+                axs[row, col].yscale("log")
+            
+            axs[row, col].grid(True)
+    
+        # set the axis labels
+        for ax in axs.flat:
+            ax.set_xlabel(x_axis, fontsize = 10)
+            ax.set_ylabel(y_axis, fontsize = 10)
+    
+        # only have axis titles on the outer edge
+        for ax in axs.flat:
+            ax.label_outer()
+    
+        # add figure title
+        fig.suptitle(title)
+       
+        # save file
+        plt.savefig(filename, dpi=800)
+    
+        return
+
 
     def plot_electric_field(self):
         '''plot the electric field at multiple timesteps'''
@@ -204,7 +354,7 @@ class Plotter:
 
        if self.params["dimensions"] == 2:
            self.plot_series(["ex", "ey"], "position.png", "Position (x)",
-               "Position (y)", "Position plot", plat.vis_util.subplot_scatter_2d)
+               "Position (y)", "Position plot", plat.vis_util.subplot_scatter_2d, plot_all=True)
 
 
     def plot_all(self):
