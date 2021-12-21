minor fixes to 2D EM PIC; added negative sign for charge in velocity update and added minor y component to single stream; created 2D ampere demonstration

py_platypus/src/py_platypus/models/pic_2d.py
@@ -141,7 +141,7 @@ class PIC_2D:
                 r = np.random.rand()
                 if r < fraction:
                     self.electron_vx[i] = v
-                    self.electron_vy[i] = 0
+                    self.electron_vy[i] = (np.random.rand() - 0.5)/1e4
         return
 
     def density_perturbation(self):

py_platypus/src/py_platypus/models/pic_2d_em.py
@@ -351,7 +351,7 @@ class PIC_2D_EM(PIC_2D):
             v_i = np.array([self.electron_vx[i], self.electron_vy[i], 0])
 
             # Boris method for updating position
-            q_prime = self.dt / 2
+            q_prime = -self.dt / 2  # negative charge b/c particle is electron
             h = np.array([0, 0, q_prime * self.b_particle[i]])
             s = 2 / (1 + np.linalg.norm(h)**2) * h
             u = v_i + q_prime * e_i