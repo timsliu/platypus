742a22e017b3531cc07081164e053cb1e0ba9925
timsliu <timliu92@gmail.com>
2021-12-24 23:28:07 -0500

    various fixes to charge_step class
    Add check that no more than 10 boundaries are being crossed (4
    along a single dimension) for each step, roughly corresponding to the
    Courant condition. Changed class to use unwrapped coordinates. Fixed
    faulty assumption for 7 boundary case that the direction with
    more boundaries is the one determining the intermediate step.

--- py_platypus/src/py_platypus/utils/charge_step.py
+++ py_platypus/src/py_platypus/utils/charge_step.py
@@ -4,16 +4,16 @@ Helper classes for updating current density in 2D EM simulations
 import numpy as np
 import py_platypus as plat
 
-
 ####################
 # Helper functions
 ####################
+
 
 def delta_midpoint(x0, dx, delta_x):
     '''
     calculates displacement to get from x0 to the nearest midpoint of
     the cell in the direction the particle is traveling. The arguments are
-    named for the x direction, but This method
+    named for the x direction, but this method
     can be used for motion in direction in any direction.
     inputs: x0 - starting coordinate
             dx - size of cell along this dimension
@@ -21,21 +21,21 @@ def delta_midpoint(x0, dx, delta_x):
     '''
 
     # wrap the coordinate so that it is inside a single cell
-    rel_x0 = x0 % dx 
+    rel_x0 = x0 % dx
 
     # particle motion in the positive direction
     if np.sign(delta_x) == 1:
-        # particle starts left half of the cells - nearest midpoint in cell 
-        if rel_x0 < dx/2: 
+        # particle starts left half of the cells - nearest midpoint in cell
+        if rel_x0 < dx / 2:
             delta_x0 = 0.5 * dx - rel_x0
-        # particle starts right half of cell - nearest midpoint is next cell 
+        # particle starts right half of cell - nearest midpoint is next cell
         else:
             delta_x0 = 1.5 * dx - rel_x0
     # particle motion in the negative direction
     else:
         # particle starts in lower half of cell - nearest midpoint in
         # previous cell
-        if rel_x0 < dx/2:
+        if rel_x0 < dx / 2:
             delta_x0 = -0.5 * dx - rel_x0
         # particle starts in upper half of cell - nearest midpoint is in
         # this cell
@@ -44,9 +44,13 @@ def delta_midpoint(x0, dx, delta_x):
 
     return delta_x0
 
+
+
+
 ####################
 # Class definitions
 ####################
+
 
 class ChargeStep:
     """
@@ -59,26 +63,26 @@ class ChargeStep:
         self.y1 = y0 + dy
         self.dx = dx
         self.dy = dy
-    
+
     def __eq__(self, c1):
         '''
         Equality operator, mostly used for testing.
         '''
         # returns whether all corresponding elements are close to each other
-        return np.logical_and.reduce(np.isclose(
-            [self.x0, self.y0, self.dx, self.dy], 
-            [c1.x0, c1.y0, c1.dx, c1.dy]
-        ))
+        return np.logical_and.reduce(
+            np.isclose([self.x0, self.y0, self.dx, self.dy],
+                       [c1.x0, c1.y0, c1.dx, c1.dy]))
 
     def __repr__(self):
         '''
         String representing the charge step
         '''
 
-        return "Charge step ({}, {})->({}, {})".format(self.x0, self.y0, self.x1, self.y1)
+        return "Charge step ({}, {})->({}, {})".format(self.x0, self.y0,
+                                                       self.x1, self.y1)
 
 
-class ChargeStepDivider: 
+class ChargeStepDivider:
     """
     Helper class that breaks the motion of a charge into a list of 
     ChargeSteps such that each ChargeStep only crosses 4 grid boundaries
@@ -87,48 +91,53 @@ class ChargeStepDivider:
         """
         initializer class
         inputs: dx - list of the sizes of each grid cell [dy, dx]
-                cells - list of the number of cells in each dimension [rows, cols]
+                cells - list of the number of cells in each dimension
+                [rows, cols]
         """
 
         self.dx = dx[1]
         self.dy = dx[0]
-            
+        self.cells = cells
+        self.xmax = self.dx * cells[1]
+        self.ymax = self.dy * cells[0]
 
-    def get_charge_steps(self, x0, y0, x1, y1): 
+    def get_charge_steps(self, x0, y0, x1, y1):
         """
         Takes the initial and final positions of a particle and returns
         a list of ChargeSteps such that each ChargeStep only crosses four
         grid boundaries
         """
+        # convert coordinates to extended, non-periodic grid
+        x0_ext, x1_ext, y0_ext, y1_ext = self.extend_coords(x0, x1, y0, y1)
 
         # list of boundaries touched at the starting and ending positions
-        hori_0, vert_0 = self.boundaries_touched(x0, y0)
-        hori_1, vert_1 = self.boundaries_touched(x1, y1)
-
-        #print("Horizontal start/end: ", hori_0, hori_1)
-        #print("Vertical start/end: ", vert_0, vert_1)
+        hori_0, vert_0 = self.boundaries_touched(x0_ext, y0_ext)
+        hori_1, vert_1 = self.boundaries_touched(x1_ext, y1_ext)
 
         # list of unique boundaries touched
         hori_all = plat.math_utils.union_lists(hori_0, hori_1)
         vert_all = plat.math_utils.union_lists(vert_0, vert_1)
 
-        #print("Horizontal: ", hori_all)
-        #print("Vertical: ", vert_all)
+        self.check_boundaries_valid(vert_all)
+        self.check_boundaries_valid(hori_all)
+
         # count unique boundaries touched at start and stop positions
         boundaries_touched = len(hori_all) + len(vert_all)
-        
+
         if boundaries_touched == 4:
             # get the list of four boundary crossing motions
-            substeps = self.get_submotions_four(x0, y0, x1, y1)
+            substeps = self.get_submotions_four(x0_ext, y0_ext, x1_ext, y1_ext)
         elif boundaries_touched == 7:
             # get the list of four boundary crossing motions
-            substeps = self.get_submotions_seven(x0, y0, x1, y1)
+            substeps = self.get_submotions_seven(x0_ext, y0_ext, x1_ext,
+                                                 y1_ext, len(hori_all))
         elif boundaries_touched == 8:
             # touching 8 unique boundaries at start and stop positions
             # corresponds to crossing 10 boundaries
-            substeps = self.get_submotions_ten(x0, y0, x1, y1)
+            substeps = self.get_submotions_ten(x0_ext, y0_ext, x1_ext, y1_ext)
         else:
-            raise ValueError("Warning! {} boundaries touched, expected 4, 7, or 8")
+            raise ValueError(
+                "Warning! {} boundaries touched, expected 4, 7, or 8")
 
         return substeps
 
@@ -138,40 +147,38 @@ class ChargeStepDivider:
         '''
         delta_x = x1 - x0
         delta_y = y1 - y0
-    
+
         return [ChargeStep(x0, y0, delta_x, delta_y)]
-    
-    def get_submotions_seven(self, x0, y0, x1, y1):
+
+    def get_submotions_seven(self, x0, y0, x1, y1, num_hori):
         '''
         returns a list of charge steps for the seven boundary case
         '''
         # determine the direction leading to two steps
-    
         delta_x = x1 - x0
         delta_y = y1 - y0
-    
-        if abs(delta_x) > abs(delta_y):
+
+        if num_hori == 3:
             # four vertical boundaries, three horizontal boundaries
             # sub motions dependent on x
 
             # moving toward or away from the midpoint
-            delta_x0 = delta_midpoint(x0, self.dx, delta_x) 
-            delta_y0 = delta_x0 * (delta_y/delta_x) 
-        
+            delta_x0 = delta_midpoint(x0, self.dx, delta_x)
+            delta_y0 = delta_x0 * (delta_y / delta_x)
+
         else:
             # four horizontal boundaries, three vertical boundaries
             # sub motions depdendent on y
-            delta_y0 = delta_midpoint(y0, self.dy, delta_y) 
-            delta_x0 = delta_y0 * (delta_x/delta_y) 
-    
+            delta_y0 = delta_midpoint(y0, self.dy, delta_y)
+            delta_x0 = delta_y0 * (delta_x / delta_y)
+
         delta_x1 = delta_x - delta_x0
         delta_y1 = delta_y - delta_y0
-    
+
         c0 = ChargeStep(x0, y0, delta_x0, delta_y0)
         c1 = ChargeStep(x0 + delta_x0, y0 + delta_y0, delta_x1, delta_y1)
-    
-        return [c0, c1]
 
+        return [c0, c1]
 
     def get_submotions_ten(self, x0, y0, x1, y1):
         '''
@@ -179,37 +186,37 @@ class ChargeStepDivider:
         '''
         delta_x = x1 - x0
         delta_y = y1 - y0
-    
+
         # fraction of the move to execute to get to the midpoint of a cell
         # along either dimension
-        x_frac = delta_midpoint(x0, self.dx, delta_x)/delta_x
-        y_frac = delta_midpoint(y0, self.dy, delta_y)/delta_y
-    
+        x_frac = delta_midpoint(x0, self.dx, delta_x) / delta_x
+        y_frac = delta_midpoint(y0, self.dy, delta_y) / delta_y
+
         # shorter to get to the x midpoint - movement to x midpoint is first
         if x_frac < y_frac:
             delta_x0 = delta_x * x_frac
             delta_y0 = delta_y * x_frac
-    
+
             delta_x1 = delta_x * y_frac - delta_x0
             delta_y1 = delta_y * y_frac - delta_y0
-    
+
         # shorter to get to the y midpoint - movement to y midpoint is first
         else:
             delta_x0 = delta_x * y_frac
             delta_y0 = delta_y * y_frac
-            
+
             delta_x1 = delta_x * x_frac - delta_x0
             delta_y1 = delta_y * x_frac - delta_y0
-    
-        delta_x2 = delta_x - delta_x1 - delta_x0 
-        delta_y2 = delta_y - delta_y1 - delta_y0 
-    
+
+        delta_x2 = delta_x - delta_x1 - delta_x0
+        delta_y2 = delta_y - delta_y1 - delta_y0
+
         c0 = ChargeStep(x0, y0, delta_x0, delta_y0)
         c1 = ChargeStep(c0.x1, c0.y1, delta_x1, delta_y1)
         c2 = ChargeStep(c1.x1, c1.y1, delta_x2, delta_y2)
-    
+
         return [c0, c1, c2]
-    
+
     def boundaries_touched(self, x, y):
         '''
         returns a list of boundaries touched
@@ -217,10 +224,60 @@ class ChargeStepDivider:
         returns: horizontal - list of indices of horizontal edges touched
                  vertical - list of indices of vertical edges touched
         '''
-        col_vert_idx = int(np.ceil((x - self.dx/2)/self.dx)) # column index of the vertical boundary
-        row_hori_idx = int(np.ceil((y - self.dy/2)/self.dy)) # row index of the horizontal boundary
+        # column index of the vertical boundary
+        col_vert_idx = int(np.ceil((x - self.dx / 2) / self.dx))
+        # row index of the horizontal boundary
+        row_hori_idx = int(np.ceil((y - self.dy / 2) / self.dy))
 
-        horizontal = [[row_hori_idx, col_vert_idx - 1], [row_hori_idx, col_vert_idx]]
-        vertical   = [[row_hori_idx - 1, col_vert_idx], [row_hori_idx, col_vert_idx]]
+        horizontal = [[row_hori_idx, col_vert_idx - 1],
+                      [row_hori_idx, col_vert_idx]]
+        vertical = [[row_hori_idx - 1, col_vert_idx],
+                    [row_hori_idx, col_vert_idx]]
 
         return horizontal, vertical
+
+    def extend_coords(self, x0, x1, y0, y1):
+        """
+        Take the coordinates from a wrapped grid space and convert them so the
+        values are as if the grid was not wrapped from (0, 0) to (xmax, ymax).
+        Only the final coordinates are modified
+        inputs: x0 - starting x
+                y0 - starting y
+                x1 - starting x
+                y1 - starting y
+        """
+
+        delta_x = x1 - x0
+        delta_y = y1 - y0
+
+        # particle transitioned from left side to right side
+        if delta_x > 0.5 * self.xmax:
+            x1 -= self.xmax
+        # particle transitioned from right side to left side
+        if delta_x < -0.5 * self.xmax:
+            x1 += self.xmax
+        # particle transitioned from low y to high y
+        if delta_y > 0.5 * self.ymax:
+            y1 -= self.ymax
+        # particle transitioned from high y to low y
+        if delta_y < -0.5 * self.ymax:
+            y1 += self.ymax
+
+        return x0, x1, y0, y1
+
+    def check_boundaries_valid(self, boundaries):
+        """
+        Check that the collection of boundaries crossed is valid. If four or
+        more unique boundaries are crossed along a single dimension, then
+        the Courant-Friedrichs-Lewy condition has been violated.
+        """
+
+        x_coors = [boundary[0] for boundary in boundaries]
+        y_coors = [boundary[1] for boundary in boundaries]
+
+        if (max(x_coors) - min(x_coors) >= 3) or (max(y_coors) - min(y_coors)
+                                                  >= 3):
+            raise RuntimeError("{} - {}\n{} \nBoundaries: {}".format(
+                "More than 10 boundaries touched",
+                "Courant condition likely violated!",
+                "Consider reducing timestep.", boundaries))
